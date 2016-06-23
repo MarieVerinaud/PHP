a6db77ea7ae1e15206f3be6749c1048064c25e7e
Marie Verinaud <marie.laugel@gmail.com>
2016-06-23 10:00:46 +0200

    Corrige un bug de caractères spéciaux dans les commentaires

--- blog_commentaire/commentaires_post.php
+++ blog_commentaire/commentaires_post.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=cours_php', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$bdd = new PDO('mysql:host=localhost;dbname=cours_php;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(Exception $e)
 {
