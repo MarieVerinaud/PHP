8b29e7f6878490cf4651c97a2521353636e12b1a
Christophe Lablancherie <c.lablancherie@live.fr>
2016-06-28 14:17:34 +0200

    Correction

--- espace_membre/controller/MembresController.class.php
+++ espace_membre/controller/MembresController.class.php
@@ -1,45 +1,85 @@
 <?php
-
+session_start();
 include_once(__DIR__ . '/../model/BDD.class.php');
 include_once(__DIR__ . '/../model/Membre.class.php');
 
 class MembresController
-{    
-    public function inscrireUnMembre()
+{
+
+    /**
+     * Cette fonction va vérifier la saisie des champs
+     */
+    public function verificationSaisie(){
+
+        if(isset($_POST["pseudo"])&& isset($_POST["eMail"]) && isset($_POST["motDePasse"]) && isset($_POST["confirmationMotDePasse"])){
+
+            $pseudo = htmlspecialchars($_POST["pseudo"]);
+            $eMail = htmlspecialchars($_POST["eMail"]);
+            $motDePasse = htmlspecialchars($_POST["motDePasse"]);
+            $confirmationMotDePasse = htmlspecialchars($_POST["confirmationMotDePasse"]);
+
+
+            if($this->alreadyRegistered($pseudo) === true){
+                $_SESSION['message'] = "Vous êtes déjà inscrit(e) !";
+                header('Location: index.php');
+            }else{
+                $this->inscrireUnMembre($pseudo, $eMail, $motDePasse, $confirmationMotDePasse);
+            }
+
+        }else{
+            $_SESSION['message'] = "Vous n'avez pas saisi tous les champs";
+        }
+
+    }
+
+    /**
+     * Cette fonction va permettre de vérifier si l'utilisateur est enregistré
+     * ou pas.
+     * @param $pseudo
+     * @return bool
+     */
+    public function alreadyRegistered($pseudo){
+
+        try
+        {
+            //ça c'est moche, faudrait une seule connexion par requête du coup
+            $bdd = (new BDD())->getConnexion();
+            $reponse = $bdd->prepare('SELECT pseudo FROM membres WHERE pseudo = :pseudo');
+            $reponse->execute(array(
+                'pseudo' => $pseudo
+            ));
+            $login = $reponse->fetch();
+
+            if (strtolower($pseudo) == strtolower($login['pseudo']))
+            {
+                return true;
+            }
+            $reponse->closeCursor();
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : '.$e->getMessage());
+        }
+
+        return false;
+    }
+
+    /**
+     * Après la vérification de la saisie, tu peux faire une inscription
+     * @param $pseudo
+     * @param $eMail
+     * @param $motDePasse
+     * @param $confirmationMotDePasse
+     */
+    public function inscrireUnMembre($pseudo, $eMail, $motDePasse, $confirmationMotDePasse)
     {
-        $pseudo = htmlspecialchars($_POST["pseudo"]);
-        $eMail = htmlspecialchars($_POST["eMail"]);
-        $motDePasse = htmlspecialchars($_POST["motDePasse"]);
-        $confirmationMotDePasse = htmlspecialchars($_POST["confirmationMotDePasse"]);
 
         $erreur = '';
         $membre = new Membre();
         $bdd = (new BDD())->getConnexion();
 
-        // Vérification si pseudo déjà utilisé
-        try 
-        {
-            $reponse = $bdd->prepare('SELECT pseudo FROM membres WHERE pseudo = :pseudo');
-            $reponse->execute(array(
-            'pseudo' => $pseudo
-            ));
-            $login = $reponse->fetch();
-        } 
-        catch (Exception $e) 
-        {
-            die('Erreur : '.$e->getMessage());
-        }
-
-        if (strtolower($pseudo) == strtolower($login['pseudo']))
-        {
-            $erreur = "Ce pseudo est déjà utilisé.";
-            $_SESSION['message'] = $erreur;
-            header('Location: index.php');
-        }
-        else
-        {
-            $membre->setPseudo($pseudo);
-        }
+        //On hydrate notre objet
+        $membre->setPseudo($pseudo);
 
 
         if (!$membre->setEmail($eMail))
@@ -56,18 +96,22 @@ class MembresController
             header('Location: index.php');
         }
 
-        $reponse->closeCursor();
 
-        $req = $bdd->prepare('INSERT INTO membres(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
+        if(!$erreur){
 
-        $req->execute(array(
-            'pseudo' => $membre->getPseudo(),
-            'pass' => $membre->getMotdePasse(),
-            'email' => $membre->getEmail()));
+            $req = $bdd->prepare('INSERT INTO membres(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
 
-        $req-> closeCursor();
-        $_SESSION['message'] = "Votre compte a bien été crée";
+            $req->execute(array(
+                'pseudo' => $membre->getPseudo(),
+                'pass' => $membre->getMotdePasse(),
+                'email' => $membre->getEmail()));
 
+            $req-> closeCursor();
+            $_SESSION['message'] = "Votre compte a bien été crée";
+
+        }else{
+            $_SESSION['message'] = "Merci de vérifier votre saisie.";
+        }
         header('Location: index.php');
     }
 }
